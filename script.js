6ab9882a0da01ec1e82e475c8212d50afc5743a2
Warshito <140309222+Warshito@users.noreply.github.com>
2023-07-28 13:27:49 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const boton = document.getElementById('miBoton');
 const texto = document.getElementById('textoUsuario');
 const resultadoDiv = document.getElementById('resultado');
 
+const boton2 = document.getElementById('miBoton2');
+
 boton.addEventListener('click', function() {
     const textUser = texto.value;
 
-    resultadoDiv.innerHTML = `You entered: ${textUser}`
+    resultadoDiv.innerHTML = `/team add ${textUser}`
+});
+
+boton2.addEventListener('click', function() {
+    const checkbx = document.querySelectorAll('input[type="checkbox"]:checked')
+    const selectedOptions = Array.from(checkbx).map(checkbox => checkbox.value);
+    alert('Seleccionaste: ' + selectedOptions.join(', '));
 });
